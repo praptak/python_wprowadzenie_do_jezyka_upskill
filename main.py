0504dd6d62c4f6bdc619193c117efd2056e8fe92
Bartlomiej Lech (PGS Software) <blech@pgs-soft.com>
2020-03-10 15:19:17 +0100

    refact

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.modules.filepath import select_file
+from src.modules.filepath import select_participants_file
 from src.modules.read_data import read_data
 from src.modules.lottery import number_of_winners, prize_drawing
 
 # select file from files folder
-selected_file = select_file()
+selected_file = select_participants_file()
 
 # number of winners
 winners_number = number_of_winners()
--- src/modules/filepath.py
+++ src/modules/filepath.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from string import Template
 
 
 # this is project's root folder - I don't know how to do it better :(
@@ -9,45 +10,50 @@ def root():
 ROOT = root()
 
 
-def searching_all_files(directory, lst):
-    dirpath = Path(directory)
-    assert (dirpath.is_dir())
-    assert all([isinstance(x, str) for x in lst])
+def searching_all_files(directory_path, file_types=None, recurse=False):
+    if file_types is None:
+        file_types = ["*"]
+    dir_path = Path(directory_path)
+    temp_file_pattern = Template("$directory*.$file_type")
+    assert (dir_path.is_dir())
+    assert all([isinstance(file_type, str) for file_type in file_types])
     file_list = []
-    [file_list.extend(dirpath.glob(f"**/*.{suffix}")) for suffix in lst]
-    lottery_files_list = []
+    [file_list.extend(dir_path.glob(temp_file_pattern.substitute(directory="**/" * recurse, file_type=file_type))) for
+     file_type in file_types]
+
+    result_files = []
 
     for num, f in enumerate(sorted(file_list), 1):
-        lottery_files_list.append(LotteryFile(num, f.resolve().name, f.resolve()))
+        result_files.append(MyFile(num, f.resolve().name, f.resolve()))
 
-    return lottery_files_list
+    return result_files
 
 
 def list_files_in_files_folder():
-    return searching_all_files((ROOT / "files/").resolve(), ['csv', 'json'])
+    return searching_all_files((ROOT / "files/participants/").resolve(), ['csv', 'json'])
 
 
-def select_file():
-    lottery_file_list = list_files_in_files_folder()
-    assert all([isinstance(x, LotteryFile) for x in lottery_file_list])
+def select_participants_file():
+    file_list = list_files_in_files_folder()
+    assert all([isinstance(x, MyFile) for x in file_list])
     selected_file = ""
-    while selected_file not in lottery_file_list:
+    while selected_file not in file_list:
         while True:
             print("Lista plików:")
-            [print(f"\t{x.id}: {x.name}") for x in lottery_file_list]
+            [print(f"\t{x.id}: {x.name}") for x in file_list]
             print()
             try:
                 input_number = int(input("Wskaż nr pliku z próbą do losowania: ")) - 1
                 if input_number < 0:
                     continue
-                selected_file = lottery_file_list[input_number]
+                selected_file = file_list[input_number]
                 print("Wybrano plik", selected_file, end="\n")
                 return selected_file
             except:
                 print("Wskaż poprawny numer!")
 
 
-class LotteryFile:
+class MyFile:
     def __init__(self, id, name, fullpath):
         self.id = id
         self.name = name
