ddb75bb182b022d967f0dbc4c20a840228a98f16
Bartlomiej Lech (PGS Software) <blech@pgs-soft.com>
2020-03-26 11:23:06 +0100

    now participant_id is int

--- lottery/participants.py
+++ lottery/participants.py
@@ -26,6 +26,6 @@ def create_list_with_weighed_participants(participants_data_list: List[Dict]) ->
     :param participants_data_list: List[Dict] - dictionary that contains data about participants
     :return: List[ParticipantWeighed]
     """
-    return [ParticipantWeighed(participant_id=record.pop("id"), weight=int(record.pop("weight", 1)), **record) for
+    return [ParticipantWeighed(participant_id=int(record.pop("id")), weight=int(record.pop("weight", 1)), **record) for
             record
             in participants_data_list]
